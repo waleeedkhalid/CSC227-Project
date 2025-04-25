318545e1c6f4491a0fbd28c310836a6da253c8df
waleeedkhalid <axd.edits@gmail.com>
2025-04-26 01:36:34 +0300

    Refactor JobScheduler to use LinkedList for job queue management; improve memory checks and job loading logic

--- src/job/JobScheduler.java
+++ src/job/JobScheduler.java
@@ -4,24 +4,28 @@ import queues.JobQueue;
 import queues.ReadyQueue;
 import utils.MemoryManager;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // Loading the jobs to ready queue should be performed in an independent thread that continuously checks
 // the available space in memory to load the next job from the job queue to the ready queue.
 public class JobScheduler implements Runnable {
 
     @Override
     public void run() {
+        Queue<PCB> jobQueue = new LinkedList<>(JobQueue.getJobQueue());
         // Load jobs from job queue to ready queue
         while (true) {
-            while (!JobQueue.isEmpty()) {
+            while (!jobQueue.isEmpty()) {
                 // Check if there is enough memory available
                 if (MemoryManager.getAvailableMemory() > 0) {
                     // Get the next job from the job queue
-                    PCB pcb = JobQueue.getNextJob();
+                    PCB pcb = jobQueue.poll();
                     if ((pcb != null) && (pcb.getRequiredMemory() <= MemoryManager.getAvailableMemory())) {
                         // Add the job to the ready queue
                         ReadyQueue.addJob(pcb);
                         // Remove the job from the job queue
-                        JobQueue.removeJob();
+                        jobQueue.remove(pcb);
                         System.out.println("Moved job to Ready Queue: " + pcb);
                     }
                 }
