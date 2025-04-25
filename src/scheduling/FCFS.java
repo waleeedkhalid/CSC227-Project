1e91314b34710eb3f6bacd1bd089e2821fcd57c4
Abdullah <444102864@student.ksu.edu.sa>
2025-04-25 19:16:15 +0300

    FCFS updated: edit run method to be load jobs from jobQueue while not empty

--- src/scheduling/FCFS.java
+++ src/scheduling/FCFS.java
@@ -2,6 +2,7 @@ package scheduling;
 
 import job.PCB;
 import job.PCBState;
+import queues.JobQueue;
 import queues.ReadyQueue;
 import utils.FileReading;
 
@@ -21,8 +22,14 @@ public class FCFS {
     public void schedule(PCB job) {
         job.setState(PCBState.RUNNING);
         System.out.println("Job ID: " + job.getId() + ", State: " + job.getState() + ", Selected at: " + currentTime + ", Starting Burst Time: " + currentTime + ", Ending Burst Time: " + (currentTime + job.getBurstTime()));
+
         currentTime += job.getBurstTime(); //25 + 13 + 20 + 10 = 68
+
         job.setState(PCBState.TERMINATED);
+        System.out.println("Job ID: " + job.getId() + ", State: " + job.getState());
+
+
+
         totalTurnaroundTime += currentTime; // 25 + 38 + 58 + 68 = 189
         totalWaitingTime += (currentTime - job.getBurstTime());// 25 - 25 = 0, 38 - 13 = 25, 58 - 20 = 38, 68 - 10 = 58, Waiting time = 0 + 25 + 38 + 58 = 121
     }
@@ -42,12 +49,23 @@ public class FCFS {
     }
 
     public void run() {
-        Queue<PCB> queue = ReadyQueue.getReadyQueue();
-        int totalJobs = queue.size();
-        while (!queue.isEmpty()) {
-            PCB job = queue.poll();
-            schedule(job);
+        int totalJobs = 0;
+
+        while (!ReadyQueue.isEmpty() || !JobQueue.isEmpty()) {
+            PCB job = ReadyQueue.removeJob(); // removes & deallocates memory
+            if (job != null) {
+                schedule(job);
+                totalJobs++;
+            } else {
+                try {
+                    Thread.sleep(100); // wait for jobs to arrive
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
         }
+
         printAverageTimes(totalJobs);
     }
 }
