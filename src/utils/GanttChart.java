f72c97fcc398906986382159805a7baa25910ecf
waleeedkhalid <axd.edits@gmail.com>
2025-04-26 23:29:20 +0300

    Refactor GanttChart class; uncomment and clean up displayGanttChart method for better readability

--- src/utils/GanttChart.java
+++ src/utils/GanttChart.java
@@ -6,49 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GanttChart {
-//    public static void displayGanttChart(List<ExecutionEvent> executionLog) {
-//        System.out.println("\nGantt Chart:");
-//        System.out.print("|");
-//
-//        for (ExecutionEvent event : executionLog) {
-//            String processId = "P" + event.getProcessId();
-//
-//            // Calculate string length
-//            int eventLength = Math.max(0, event.endTime - event.startTime - processId.length() + 1);
-//
-//            // Print process ID centered
-//            int padding = Math.max(0, eventLength - processId.length());
-//            int leftPad = Math.max(0, padding / 2);
-//            int rightPad = Math.max(0, padding - leftPad);
-//
-//            for (int i = 0; i < leftPad; i++) {
-//                System.out.print(" ");
-//            }
-//            System.out.print(processId);
-//            for (int i = 0; i < rightPad; i++) {
-//                System.out.print(" ");
-//            }
-//            System.out.print("|");
-//        }
-//
-//        System.out.print("\n0");
-//
-//        // Print time markers
-//        int lastPosition = 0;
-//        for (ExecutionEvent event : executionLog) {
-//            String timeMarker = Integer.toString(event.endTime);
-//            int position = lastPosition + (event.endTime - event.startTime) + 1;
-//
-//            for (int i = 0; i < position - lastPosition - timeMarker.length(); i++) {
-//                System.out.print(" ");
-//            }
-//
-//            System.out.print(timeMarker);
-//            lastPosition = position;
-//        }
-//        System.out.println();
-//    }
-
     public static void displayGanttChart(List<ExecutionEvent> executionLog) {
         System.out.println("\nGantt Chart:");
 
